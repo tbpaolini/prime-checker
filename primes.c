12cb9aa89a6300a063ecfe6733cd89e3999de360
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-01-14 15:03:00 -0300

    Implementation of the Sieve of Erastothenes

--- primes.c
+++ primes.c
@@ -6,6 +6,14 @@
 #include <math.h>
 #include <ctype.h>
 
+// An array of prime numbers
+typedef struct PrimesArray
+{
+    uint64_t length;    // Amount of elements
+    uint64_t primes[];  // Elements
+} PrimesArray;
+
+// Determine if a given positive integer is prime
 static bool is_prime(uint64_t value)
 {
     // Special cases:
@@ -28,6 +36,62 @@ static bool is_prime(uint64_t value)
     }
     
     return true;
+}
+
+static inline bool sieve_get(uint8_t *sieve, size_t byte, uint8_t bit)
+{
+    return sieve[byte] & (1 << bit);
+}
+
+static inline void sieve_set(uint8_t *sieve, size_t byte, uint8_t bit)
+{
+    sieve[byte] | (1 << bit);
+}
+
+// Returns an array of prime numbers whithin a range
+static PrimesArray *primes_range(uint64_t start, uint64_t end)
+{
+    // Swap the values when "end" is smaller than "start"
+    if (end < start)
+    {
+        start ^= end;
+        end ^= start;
+        start ^= end;
+    }
+
+    const uint64_t range_len = end - start;
+    size_t buffer_size = range_len / 8UL;
+    if (range_len % 8 != 0) buffer_size += 1UL;
+
+    // Sieve of Erastothenes
+    // This is an array of bitmasks to flag if a number is not prime
+    // Each bit represents a number, so a byte represent 8 numbers.
+    // If a bit is set, then the corresponding number is not prime.
+    uint8_t *sieve = (uint8_t*)calloc(buffer_size, sizeof(uint8_t));
+
+    for (uint64_t value = start; value <= end; value++)
+    {
+        // Calculate the position of the value on the sieve
+        const uint64_t offset = value - start;
+        const size_t byte = offset / 8UL;
+        const uint8_t bit = offset % 8UL;
+
+        // Check if the current value was already flagged as not being prime
+        if (sieve_get(sieve, byte, bit)) continue;
+
+        // Check if the value is prime
+        if (is_prime(value))
+        {
+            // If the value is prime, flag its multiples as not being prime
+            for (uint64_t multiple = value * 2; multiple <= end; multiple += value)
+            {
+                const uint64_t offset = multiple - start;
+                const size_t byte = offset / 8UL;
+                const uint8_t bit = offset % 8UL;
+                sieve_set(sieve, byte, bit);
+            }
+        }
+    }
 }
 
 static bool validate_input(char *input)
